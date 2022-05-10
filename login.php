32f55f86463c22672135e8f8c968038879b40451
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-09 21:22:55 -0400

    add a login form

--- login.php
+++ login.php
@@ -5,3 +5,24 @@ session_start();
 $_SESSION['is_logged_in'] = true;
 
 ?>
+
+<?php require 'includes/header.php'; ?>
+
+<h2>Login</h2>
+
+<form>
+
+    <div>
+        <label for="username">Username</label>
+        <input name="username" id="username">
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+    </div>
+
+    <button>Log in</button>
+
+</form>
+
